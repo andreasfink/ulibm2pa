2ee2f58a7b48df06521a018b04521ba6b299db6f
Andreas Fink <afink@amdtelecom.net>
2018-11-29 13:45:37 +0200

    fix

--- Classes/UMM2PALinkStateControl_NotAligned.h
+++ Classes/UMM2PALinkStateControl_NotAligned.h
@@ -6,12 +6,10 @@
 //  Copyright © 2018 Andreas Fink (andreas@fink.org). All rights reserved.
 //
 
-#import <ulibm2pa/ulibm2pa.h>
+#import "UMM2PALinkStateControl_State.h"
 
-NS_ASSUME_NONNULL_BEGIN
 
 @interface UMM2PALinkStateControl_NotAligned : UMM2PALinkStateControl_State
 
 @end
 
-NS_ASSUME_NONNULL_END
