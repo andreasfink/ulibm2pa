6fc5ecf0dccb38110a7ccd6002f64d8f7cc45d9b
Andreas Fink <andreas@fink.org>
2021-07-17 21:14:21 +0200

    wip

--- Classes/UMLayerM2PAStatus.h
+++ Classes/UMLayerM2PAStatus.h
@@ -13,14 +13,14 @@
 
 typedef	enum	M2PA_Status
 {
-    M2PA_STATUS_FOOS                 = 98,  /* configured to stay off. Forced out of service */
-    M2PA_STATUS_DISCONNECTED         = 99,  /* connection not yet requested */
-    M2PA_STATUS_OFF                  = 100, /* connection requested but SCTP is not yet up */
-    M2PA_STATUS_OOS					 = 101, /* sctp up. waiting for MTP3 to start it*/
-    M2PA_STATUS_INITIAL_ALIGNMENT	 = 102, /* alignment sent to remote. await alignment from remote */
-    M2PA_STATUS_ALIGNED_NOT_READY	 = 103, /* proving phase running. not enough proving received yet */
-    M2PA_STATUS_ALIGNED_READY		 = 104, /* proving phase running. enough proving received but remote is not yet happy */
-    M2PA_STATUS_IS					 = 105, /* both sides where ready . live for traffic*/
+    M2PA_STATUS_FOOS                 = 998,  /* configured to stay off. Forced out of service */
+    M2PA_STATUS_DISCONNECTED         = 999,  /* connection not yet requested */
+    M2PA_STATUS_OFF                  = 1000, /* connection requested but SCTP is not yet up */
+    M2PA_STATUS_OOS					 = 1001, /* sctp up. waiting for MTP3 to start it*/
+    M2PA_STATUS_INITIAL_ALIGNMENT	 = 1002, /* alignment sent to remote. await alignment from remote */
+    M2PA_STATUS_ALIGNED_NOT_READY	 = 1003, /* proving phase running. not enough proving received yet */
+    M2PA_STATUS_ALIGNED_READY		 = 1004, /* proving phase running. enough proving received but remote is not yet happy */
+    M2PA_STATUS_IS					 = 1005, /* both sides where ready . live for traffic*/
 } M2PA_Status;
 
 
