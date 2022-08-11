23f2ea83b0b7de2cc593e06e3de0ff7a6de05e45
Andreas Fink <andreas@fink.org>
2022-08-11 20:32:17 +0200

    work in progress

--- Classes/UMLayerM2PA.m
+++ Classes/UMLayerM2PA.m
@@ -2138,7 +2138,7 @@
                 {
                     NSLog(@"LastEvents%d: %@",i,_lastEvent[0]);
                 }
-                usleep(2);
+                sleep(1);
                 return -1;
             case UMSOCKET_STATUS_OOS:
                 [self logDebug:[NSString stringWithFormat:@"Can not send %@ due to UMSOCKET_STATUS_OOS",ls ]];
